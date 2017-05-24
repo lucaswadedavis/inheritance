287260b94523e9fa164da70ee1691aa218a2c227
lucaswadedavis <lucaswadedavis@gmail.com>
2017-05-24 09:08:13 -0700

    Make the first slider work.

--- js/app.js
+++ js/app.js
@@ -3,40 +3,107 @@ $(document).ready(function(){init();});
 ////////////////////////////////////////////////
 
 function init() {
- $('#heritability').slider(state.heritability) 
- $('#polygenaity').slider(state.polygenaity) 
- $('#assortative-mating').slider(state.assortativeMatig) 
+ $('#population-size').slider(state.populationSize);
+ $('#number-of-generations').slider(state.numberOfGenerations);
+ $('#heritability').slider(state.heritability);
+ $('#polygenaity').slider(state.polygenaity);
+ $('#assortative-mating').slider(state.assortativeMating);
+ $('button').on('click', calculate);
 }
 
-var state = {
+window.state = {
+  populationSize: {min:2, max: 200, value: 100},
+  numberOfGenerations: {min:10, max: 1000, value: 100},
   polygenaity: {min:1, max:100, value:50},
-  heritability: {min:0, max:1, value:0.5},
-  assortativeMating: {min:-1, max:1, value:0}
+  heritability: {min:0, max:100, value:50},
+  assortativeMating: {min:-100, max:100, value:0}
 }
 
-////////////////////////////////////////////////
+state.heritability.slide = function(event, ui) {
+  state.heritability.value = ui.value;
+  $("#heritability-title").text("Heritability: " + ui.value);
+};
 
-function sliders(bounds, context) {
-  _.each(app.m.variables, function(x) {
-    $('div.' + x.id).slider({
-      value: x.min,
-      min: x.min, 
-      max: x.max,
-      slide: function(event, ui) {
-        $('span.' + x.id).text(ui.value + ' ' + x.units);
-        app.m[x.id] = ui.value;
-        var meanSurfaceTemperature = app.c.calculateMeanSurfaceTemperature(app.m.solarInput, app.m.meanSurfacePressure);
-        tempF = ((meanSurfaceTemperature * 9) / 5) -459.67;
 
-        if (!_.isNaN(meanSurfaceTemperature)) {
-          $('span#mean-surface-temperature').text(Math.floor(tempF) + ' degrees F');
-        }
+state.assortativeMating.slide = function(event, ui) {
+  state.assortativeMating.value = ui.value;
+  $("#assortative-mating-title").text("Assortative Mating: " + ui.value);
+};
 
-        $('input[type=text].' + x.id).val(ui.value);
 
-      }
-    });
-  });
+state.polygenaity.slide = function(event, ui) {
+  state.polygenaity.value = ui.value;
+  $("#polygenaity-title").text("Polygenaity: " + ui.value);
+};
 
+function breed(a, b) {
+  // start with perfect heritability
+  var x = {iq: 0, genes: []};
+  for (var i = 0; i < a.genes.length; i++) {
+    var gene = Math.random() > 0.5 ? a.genes[i] : b.genes[i];
+    x.iq += gene;
+    x.genes.push(gene);
+  }
+  x.iq /= state.polygenaity.value;
+  // heritability stuff goes here
+  return x;
+};
+
+function selectMateAndBreed(organisms) {
+  // start with perfectly assortative mating
+  organisms.sort((a, b) => a.iq - b.iq);
+  var a = [];
+  var b = [];
+  var nextGeneration = [];
+  for (var i = 0; i < organisms.length; i++) {
+    i % 2 === 0 ? b.push(organisms[i]) : a.push(organisms[i]); 
+  };
+
+  for (var i = 0; i < a.length; i++) {
+    // a is odd and never shorter than b
+    // assume a stable population, so each breeding pair would produce two offspring
+    // this too may be a good target for manipulation later
+    nextGeneration.push(breed(a[i], b[i]));
+    nextGeneration.push(breed(a[i], b[i]));
+  }
+
+  return nextGeneration;
+};
+
+function Organism() {
+  var x = {iq: 0, genes: []};
+  for (var i = 0; i < state.polygenaity.value; i++) {
+    var gene = Math.random();
+    x.genes.push(gene);
+    x.iq += gene;
+  }
+
+  x.iq /= state.polygenaity.value;
+
+  return x;
+}
+
+function calculate() {
+  var populationSize = 100;
+  var numberOfGenerations = 100;
+  var generations = [];
+  var currentGeneration = []; 
+  for (var i = 0; i < populationSize; i++) {
+    currentGeneration.push(Organism());
+    generations.push(currentGeneration);
+  }
+  console.log(currentGeneration[0]);
+
+  for (var i = 0; i < numberOfGenerations; i++) {
+    currentGeneration = selectMateAndBreed(currentGeneration);
+    console.log(
+      "Generation " + i,
+      currentGeneration[0].iq,
+      currentGeneration[currentGeneration.length - 1].iq
+    );
+    generations.push(currentGeneration);
+  }
+
+}
 
 };
